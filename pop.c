69edc0103ce56a6dfa6a460015203e6c17272d70
kyalo460 <kyalo460@gmail.com>
2023-09-21 16:03:28 +0000

    Made sure to delete correct node

--- pop.c
+++ pop.c
@@ -7,17 +7,13 @@
   */
 void monty_pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *next = NULL;
+	stack_t *current = *stack;
+	stack_t *del;
 
-	if ((*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	next = (*stack)->next->next;
-	free((*stack)->next);
-	if (next)
-		next->prev = *stack;
-	(*stack)->next = next;
+	del = current;
+	current = current->next;
+	free(del);
+	if (current)
+		current->prev = NULL;
+	*stack = current;
 }
